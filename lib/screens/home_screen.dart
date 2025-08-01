a9d3d8b4c907585a4cb9c86d1ac8f669ddd2b4f4
Jodunn1 <jordandunndeveloping@gmail.com>
2025-08-01 16:02:37 -0700

    Changed top nav bar, added drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:toury_app/widgets/bottom_nav_bar.dart';
 import 'package:toury_app/widgets/top_nav_bar.dart';
+import 'package:toury_app/widgets/drawer.dart';
+
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TopNavBar(),
+      drawer: CustomDrawer(),
+      body:TopNavBar(),
       // Add side buttons here later
       bottomNavigationBar: BottomNavBar(),
     );
--- lib/widgets/top_nav_bar.dart
+++ lib/widgets/top_nav_bar.dart
@@ -2,55 +2,45 @@ import 'package:flutter/material.dart';
 
 class TopNavBar extends StatelessWidget {
   const TopNavBar({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Stack(
         children: [
           Positioned(
-            top: 60,
+            top: 62,
             left: 36,
             right: 36,
-            child: Material(
-              elevation: 0,
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.blue,
-              child: Container(
-                height: 43,
-                padding: const EdgeInsets.symmetric(horizontal: 6),
-                alignment: Alignment.centerLeft,
-                child: Row(
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.menu),
-                      color: Colors.white,
-                      onPressed: () {
-                      },
-                    ),
-                    SizedBox(width: 85),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Search',
-                          hintStyle: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ],
+            child: Expanded(
+              child: SizedBox(
+                height: 40,
+                child: SearchBar(
+                hintText: 'Ask me anything',
+                backgroundColor: MaterialStateProperty.all(Color(0xFF0F3966).withOpacity(0.8)),
+                textStyle: MaterialStateProperty.all(const TextStyle(color: Colors.white)),
+                hintStyle: MaterialStateProperty.all(const TextStyle(color: Colors.white)),
+                elevation: MaterialStateProperty.all(0),
+                leading: Padding(
+                  padding: const EdgeInsets.only(right: 40.0),
+                  child: DrawerButton(
+                            color: Colors.white,
+                            onPressed: () {
+                              Scaffold.of(context).openDrawer();
+                            },
+                          ),
                 ),
               ),
+              ),
             ),
-          ),
-          Stack(
-            children: [
-              Positioned(
+              ),
+          Positioned(
                 top: 55,
                 left: 290,
                 right: 0,
                 child: Container(
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    border: Border.all(color: Colors.red, width: 1.5),
+                    border: Border.all(color: Colors.white, width: 1.5),
                   ),
                   child: CircleAvatar(
                     radius: 25,
@@ -58,8 +48,6 @@ class TopNavBar extends StatelessWidget {
                   ),
                 )
               ),
-            ],
-          ),
         ],
     );
 }
